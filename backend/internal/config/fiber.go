c82801b1524bba4842be254f341ce807936f81a9
ekaadev <ekaadev9@gmail.com>
2026-01-28 20:45:06 +0700

    feat: dynamically configure CORS origin using `CORS_ORIGIN` environment variable

--- backend/internal/config/fiber.go
+++ backend/internal/config/fiber.go
@@ -19,7 +19,7 @@ func NewFiber(viper *viper.Viper) *fiber.App {
 
 	// CORS middleware untuk mengizinkan request dari frontend
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5173",
+		AllowOrigins:     viper.GetString("CORS_ORIGIN"),
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders:     "Origin,Content-Type,Accept,Authorization",
 		AllowCredentials: true,
